16fa01c2e83e967dd8fa2c9d302f1d76b883b78b
FIORELLA <fiogeminimay@gmail.com>
2025-08-06 02:47:14 -0500

    modificado   generar-pdf route por laa non key

--- src/app/api/ficha/generar-pdf/route.ts
+++ src/app/api/ficha/generar-pdf/route.ts
@@ -8,7 +8,8 @@ const prisma = new PrismaClient()
 
 // Configuración de Supabase
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+// const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export async function POST(request: NextRequest) {
